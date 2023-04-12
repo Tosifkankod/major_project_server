afe514f8ce1600fd470440e143586116fc949509
Tosif Kankod <tosifkankod146@gmail.com>
2023-04-12 22:41:40 +0530

    Commited for path error

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -5,7 +5,7 @@ import multer from 'multer';
 export const uploadSlider = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./Data");
+      cb(null, "../Data");
     },
     filename: function (req, file, cb) {
       console.log(file);
