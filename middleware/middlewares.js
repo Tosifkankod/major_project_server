8d0d1a590103f1c2fd643b5d888bf87a49f10734
Tosif Kankod <tosifkankod146@gmail.com>
2023-04-14 11:39:46 +0530

    Commited after adding savefilepath to multer for slider

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -1,11 +1,18 @@
 import multer from 'multer';
+import { fileURLToPath } from 'url';
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const savefilePath = path.join(__dirname, "../Data/");
 
 
 //Middleware for Slider Image
 export const uploadSlider = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./Data");
+      console.log(savefilePath);
+      cb(null, savefilePath);
     },
     filename: function (req, file, cb) {
       console.log("slider multer log", file);  
